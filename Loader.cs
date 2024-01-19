82379da3bed3a02f84440e4e981101adbca3ab55
Lune3x3 <robsmith.exe@gmail.com>
2024-01-19 16:06:04 -0500

    Main commit

---
+++ Loader.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using MelonLoader;
+using UnityEngine;
+using UnityEditor;
+using Unity;
+
+namespace LethalCompMod
+{
+    public class Loader : MelonMod
+    {
+        public static bool staminaHack = false;
+        public static bool ESP = false;
+        public static float reachHack = 0.0f;
+        public static bool noWeight = false;
+        public static bool magnetHack = false;
+
+        public override void OnUpdate()
+        {
+            Mods.staminaHack();
+            Mods.reachHack();
+            Mods.noWeight();
+        }
+
+        public override void OnGUI()
+        {
+            /*
+            GUI.Box(new Rect(25, 25, 200, 200), "Test Box");
+
+            if (GUI.Button(new Rect(25, 30, 195, 40), "stamina hack"))
+            {
+                staminaHack = !staminaHack;
+            }
+
+            if (GUI.Button(new Rect(27, 80, 195, 40), "ESP"))
+            {
+                ESP = !ESP;
+            }
+            */
+
+            GUI.Window(0, new Rect(25, 25, 400, 400), menuFunction, "Mod Window");
+
+            Mods.ESP();
+        }
+
+        public void menuFunction(int WindowID)
+        {
+            var player = GameNetworkManager.Instance.localPlayerController;
+
+            staminaHack = GUI.Toggle(new Rect(27, 30, 195, 40), staminaHack, "Infinite stamina");
+            ESP = GUI.Toggle(new Rect(27, 80, 195, 40), ESP, "Wall Hacks");
+            GUI.Label(new Rect(27, 130, 195, 20), "Increase Reach");
+            reachHack = GUI.HorizontalSlider(new Rect(27, 150, 195, 20), reachHack, 5f, 50f);
+            noWeight = GUI.Toggle(new Rect(27, 180, 195, 40), noWeight, "No Weight");
+
+            if (player != null)
+            {
+                // Y is up and down for position
+                Vector3 playerPos = player.transform.position;
+                GUI.Label(new Rect(27, 230, 195, 40), "X: " + playerPos.x.ToString() + " Y: " + playerPos.y.ToString() + " Z: " + playerPos.z.ToString());
+
+                // Y is left and right for looking
+                Vector3 playerLook = player.transform.eulerAngles;
+                GUI.Label(new Rect(27, 280, 195, 40), "x: " + playerLook.x.ToString() + " y: " + playerLook.y.ToString() + " z: " + playerLook.z.ToString());
+            }
+
+            GUI.DragWindow(new Rect(0, 0, 10000, 10000));
+        }
+    }
+}
---
+++ Mods.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+using UnityEditor;
+using Unity;
+
+
+namespace LethalCompMod
+{
+    class Mods
+    {
+        public static void staminaHack()
+        {
+            if (Loader.staminaHack)
+            {
+                GameNetworkManager.Instance.localPlayerController.sprintMeter = 1f;
+            }
+        }
+
+        public static bool isOnScreen(Vector3 position, Camera camera)
+        {
+            if (position.x < 0f || position.x > camera.pixelWidth || position.y < 0f || position.y > camera.pixelHeight || position.z < 0f) return false;
+            return true;
+        }
+
+        public static void ESP()
+        {
+            if (Loader.ESP)
+            {
+                var camera = GameNetworkManager.Instance.localPlayerController.gameplayCamera;
+                var player = GameNetworkManager.Instance.localPlayerController;
+
+                var ScreenScale = new Vector2((float)Screen.width / camera.pixelWidth, (float)Screen.height / camera.pixelHeight);
+                var ScreenCenter = new Vector2((float)(Screen.width / 2), (float)(Screen.height - 1));
+
+                var enemies = GameObject.FindObjectsOfType<EnemyAI>();
+
+                if (enemies == null)
+                {
+                    return;
+                }
+
+                foreach (var enemy in enemies)
+                {
+                    if (enemy == null || enemy.isEnemyDead)
+                    {
+                        continue;
+                    }
+
+                    Vector3 pivotPos = enemy.transform.position;
+                    Vector3 screenPos = camera.WorldToScreenPoint(pivotPos);
+
+                    if (!isOnScreen(screenPos, camera))
+                    {
+                        continue;
+                    }
+
+                    var name = enemy.enemyType;
+                    var distance = Vector3.Distance(player.transform.position, enemy.transform.position);
+
+                    Vector2 vec2Pos = new Vector2(screenPos.x * ScreenScale.x, (float)Screen.height - (screenPos.y * ScreenScale.y));
+
+                    Color color = Color.red;
+                    string text = "" + name + ": " + distance;
+
+                    Render.DrawString(new Vector2(vec2Pos.x, vec2Pos.y - 20f), text, true);
+                    Render.DrawLine(ScreenCenter, vec2Pos, color, 2f);
+                }
+            }
+        }
+
+        public static void reachHack()
+        {
+            if (Loader.reachHack > 5f)
+            {
+                GameNetworkManager.Instance.localPlayerController.grabDistance = Loader.reachHack;
+            }
+        }
+
+        public static void noWeight()
+        {
+            if (Loader.noWeight)
+            {
+                GameNetworkManager.Instance.localPlayerController.carryWeight = 1f;
+            }
+        }
+
+        public static void magnet()
+        {
+            if (Loader.magnetHack)
+            {
+                var camera = GameNetworkManager.Instance.localPlayerController.gameplayCamera;
+                var player = GameNetworkManager.Instance.localPlayerController;
+
+                var items = GameObject.FindObjectsOfType<GrabbableObject>();
+
+                var ScreenScale = new Vector2((float)Screen.width / camera.pixelWidth, (float)Screen.height / camera.pixelHeight);
+                var ScreenCenter = new Vector2((float)(Screen.width / 2), (float)(Screen.height - 1));
+
+                if (items == null || items.Length == 0)
+                {
+                    return;
+                }
+
+                foreach (var item in items)
+                {
+                    if (item == null || item.isHeld || !item.grabbable || item.isInShipRoom || item.isInElevator || !item.isInFactory)
+                    {
+                        continue;
+                    }
+
+                    Vector3 itemPos = item.transform.position;
+
+                    Vector3 playerLook = camera.transform.forward;
+                    Vector3 playerLookOrigin = camera.transform.position;
+
+                    Vector3 itemToPlayerPos = itemPos - playerLookOrigin;
+                    //change playerLook to look at item, then use reflection to call BeginGrabObject
+
+                    RaycastHit hit;
+                    if (Physics.Raycast(playerLookOrigin, itemToPlayerPos, out hit, player.grabDistance))
+                    {
+                        if (hit.transform.position == itemPos)
+                        {
+                            camera.transform.LookAt(itemPos);
+                            // use reflection here plz
+
+                            camera.transform.forward = playerLook;
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
